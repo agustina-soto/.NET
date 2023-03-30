1458770130d35c2f1806fa318a05a004171773e6
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-03-30 12:02:29 -0300

    Create ejercicio2.cs

---
+++ Practica 3/ejercicio2.cs
@@ -0,0 +1,26 @@
+/*
+Implementar un método para imprimir por consola todos los elementos de una matriz (arreglo
+de dos dimensiones) pasada como parámetro. Debe imprimir todos los elementos de una fila en
+la misma línea en la consola.
+*/
+
+using System;
+
+void imprimirMatriz(int[,] matriz) {
+    for (int i = 0; i < matriz.GetLength(0); i++){
+        for (int j = 0; j < matriz.GetLength(1); j++){
+            Console.Write(matriz[i, j] + " - ");
+        }
+        Console.WriteLine("\n");
+    }
+}
+
+int [,] matriz = new int[,]
+{
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12}
+};
+
+imprimirMatriz(matriz);
+Console.ReadLine();
