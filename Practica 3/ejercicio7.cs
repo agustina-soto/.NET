e7ba3da562e17913c97a3007f28b8004a4a51537
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-03-31 11:08:22 -0300

    Create ejercicio7.cs

---
+++ Practica 3/ejercicio7.cs
@@ -0,0 +1,9 @@
+int i = 10;
+var x = i * 1.0;
+var y = 1f;
+var z = i * y;
+
+Console.WriteLine(i.GetType()); // System.Int32
+Console.WriteLine(x.GetType()); // System.Double
+Console.WriteLine(y.GetType()); // System.Single
+Console.WriteLine(z.GetType()); // System.Single
