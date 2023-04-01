c008408721800a3211913a58b6a72da1c89f1309
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-01 20:49:57 -0300

    Create ejercicio13_OP1.cs

---
+++ Practica 3/ejercicio13_OP1.cs
@@ -0,0 +1,51 @@
+/*
+13. Realizar un análisis sintáctico para determinar si los paréntesis en una expresión aritmética están
+bien balanceados. Verificar que por cada paréntesis de apertura exista uno de cierre en la cadena
+de entrada. Utilizar una pila para resolverlo. Los paréntesis de apertura de la entrada se
+almacenan en una pila hasta encontrar uno de cierre, realizándose entonces la extracción del
+último paréntesis de apertura almacenado. Si durante el proceso se lee un paréntesis de cierre y
+la pila está vacía, entonces la cadena es incorrecta. Al finalizar el análisis, la pila debe quedar
+vacía para que la cadena leída sea aceptada, de lo contrario la misma no es válida.
+*/
+using System; using System.Collections.Generic;
+
+bool estaBalanceada(string exp, Stack<char> pila){
+    bool ok = true;
+    for (int i = 0; i < exp.Length; i++)
+    {
+        if (exp[i] == '(') // Si encontro un parentesis de apertura
+        {
+            pila.Push(exp[i]); // Pushear en la pila
+        }
+        else if (exp[i] == ')') // Si encontro un parentesis de cierre
+        {
+            if (pila.Count > 0){
+                pila.Pop(); // Popear de la pila
+            }
+            else{
+                ok = false;
+            }
+        }
+    }
+    return ok;
+}
+
+bool esValida(Stack<char> pila){
+    return pila.Count == 0;
+}
+
+string exp = "((())";
+Stack<char> pila = new Stack<char>();
+
+if (estaBalanceada(exp, pila))
+{
+    Console.Write("La expresion aritmetica esta balanceada");
+    if (!esValida(pila))
+    {
+        Console.WriteLine(", pero la cadena no es valida");
+    }
+}
+else
+{
+    Console.WriteLine("La expresion aritmetica no esta balanceada");
+}
