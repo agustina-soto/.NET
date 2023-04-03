696b5eb3d105b4d22cf9d22c6e36617f5b7cbebc
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-02 22:05:27 -0300

    Update ejercicio12.cs

--- Practica 3/ejercicio12.cs
+++ Practica 3/ejercicio12.cs
@@ -1,15 +1,15 @@
 /*
 ME IMPRIME "HOLAAMUNDO" :(
-No se que toque y ahora me tira error en index
 */
 
-
-using System; using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 /* Genera una tabla de caracteres con el abecedario y el space*/
 char[] generarTabla(){
     char caracter = 'A';
-    char[] tabla = new char[28];
+    char[] tabla = new char[27]; // .net no considera la ñ: son 26 letras + el space
     int i;
     
     for (i = 0; i < tabla.Length-1; i++, caracter++)
@@ -19,6 +19,13 @@ char[] generarTabla(){
     tabla[i] = ' ';
 
     return tabla;
+}
+
+void imprimirTabla(char[] tabla){
+    foreach(char c in tabla)
+    {
+        Console.WriteLine(c);
+    }
 }
 
 /* Devuelve una cola de enteros cargada con una clave recibida por parametro */
@@ -54,14 +61,14 @@ StringBuilder cifrarMensaje(char[] tabla, Queue<int> clave, string msj){
     for (int i = 0; i < msj.Length; i++)
     {
         pos = buscarPos(tabla, msj[i]); // Busca la pos donde se encuentra el caracter actual
-        
         elem = clave.Dequeue(); // Saca elem de la cola
         clave.Enqueue(elem); // Vuelve a encolar elem
-        
         elem += pos;
-        
-        if (elem > 28) elem -= 28;
-        
+        if (elem == 27) Console.WriteLine($"este es el space = {elem}");
+        if (elem > 26){
+            elem -= 26; // pos maxima a la que acceder = 26
+        }
+        if (elem == 1) Console.WriteLine($"este es el space actualizado = {elem}");
         sb.Append(tabla[elem]);
     }
     return sb; // Queda cargado un string de char modificable
@@ -76,14 +83,16 @@ StringBuilder descifrarMensaje(char[] tabla, Queue<int> clave, StringBuilder msj
     for (int i = 0; i < msjCifrado.Length; i++)
     {
         pos = buscarPos(tabla, msjCifrado[i]); // Busca la pos donde se encuentra el caracter actual
-
+        Console.WriteLine($"pos = {pos}");
         elem = clave.Dequeue(); // Saca elem de la cola
         clave.Enqueue(elem); // Vuelve a encolar elem
-        
+        Console.WriteLine($"elem = {elem}");
         pos -= elem;
-        
-        if (pos < 0) pos += 28;
-
+        Console.WriteLine($"resta = {pos}");
+        if (pos < 0){
+            pos += 26;
+            Console.WriteLine($"pos actuaizada = {pos}");
+        }
         sb.Append(tabla[pos]);
     }
     return sb;
@@ -92,13 +101,13 @@ StringBuilder descifrarMensaje(char[] tabla, Queue<int> clave, StringBuilder msj
 string mensaje = "HOLA MUNDO";
 List<int> clave = new List<int>() {1, 2, 3, 4};
 char[] tabla = generarTabla();
+imprimirTabla(tabla);
 Queue<int> cola = generarClaveRepetida(clave);
 
 StringBuilder mensajeCifrado = cifrarMensaje(tabla, generarClaveRepetida(clave), mensaje);
 Console.WriteLine($"Mensaje cifrado: {mensajeCifrado}");
 StringBuilder mensajeDescifrado = descifrarMensaje(tabla, generarClaveRepetida(clave), mensajeCifrado);
 Console.WriteLine($"Mensaje descifrado: {mensajeDescifrado}");
-
 
 
 /*
