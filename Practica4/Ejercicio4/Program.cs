869819d4769a509f5c4029d389f78350d7bc019d
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-13 22:49:29 -0300

    Update Program.cs

--- Practica4/Ejercicio4/Program.cs
+++ Practica4/Ejercicio4/Program.cs
@@ -1,1 +1,8 @@
+/*
+Codificar la clase Hora de tal forma que el siguiente código produzca la salida por consola que se observa.
+*/
+using System;
+using ejercicio4;
 
+Hora h = new Hora(23,30,15);
+h.Imprimir();
