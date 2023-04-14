7374a6178d5e8d0eaca257e6b0e2ec42016c28c6
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-13 22:30:19 -0300

    Create Persona.cs

---
+++ Practica4/Ejercicio3/Persona.cs
@@ -0,0 +1,52 @@
+namespace ejercicio3;
+
+class Persona
+{
+    private String _nombre;
+    private int _edad;
+    private String _DNI;
+
+    public Persona(){
+
+    }
+
+    public Persona(int edad){
+        this._edad = edad;
+    }
+
+    public Persona(String nombre, int edad, String DNI){
+        this._nombre = nombre;
+        this._edad = edad;
+        this._DNI = DNI;
+    }
+
+    public void setNombre(String nombre){
+        this._nombre = nombre;
+    }
+
+    public void setEdad(int edad){
+        this._edad = edad;
+    }
+    public void setDNI(String DNI){
+        this._DNI = DNI;
+    }
+    public String getNombre(){
+        return this._nombre;
+    }
+
+    public int getEdad(){
+        return this._edad;
+    }
+    public String getDNI(){
+        return this._DNI;
+    }
+
+    public bool EsMayorQue(Persona p){
+        return ( this.getEdad() > p.getEdad() );
+    }
+
+    public String Imprimir(){
+        String st = $"{_nombre}\t{_edad}\t{_DNI}";
+        return st;
+    }
+}
