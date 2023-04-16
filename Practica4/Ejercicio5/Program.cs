29d76eacc74b97c85e81b9b109dedfd861d1aef2
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-15 22:07:13 -0300

    Update Program.cs

--- Practica4/Ejercicio5/Program.cs
+++ Practica4/Ejercicio5/Program.cs
@@ -1,1 +1,15 @@
+using System;
+using ejercicio5;
 
+Hora h = new Hora(23, 30, 15);
+h.Imprimir();
+h = new Hora(14.43);
+h.Imprimir();
+h = new Hora(14.45);
+h.Imprimir();
+h = new Hora(14.45114);
+h.Imprimir();
+h = new Hora(14.99999999);
+h.Imprimir();
+h = new Hora(16.59999999);
+h.Imprimir();
