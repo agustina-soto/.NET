446462a40633fb97b9e1d251995841315f4944da
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-16 00:27:30 -0300

    Update Programa1.cs

--- Practica4/Ejercicio9/Programa1.cs
+++ Practica4/Ejercicio9/Programa1.cs
@@ -2,9 +2,13 @@ Foo f = new Foo();
 f.Imprimir();
 class Foo
 {
-string? _bar;
-public void Imprimir()
-{
-Console.WriteLine(_bar.Length);
-}
+    string? _bar; // string? es lo mismo que string solo, porque los string aceptan valores nulos
+    public void Imprimir()
+    {
+        Console.WriteLine(_bar.Length); /* Da error por excepcion no manejada. Como no se inicializo
+                                        manualmente elvalor de _bar se hizo por defecto. Los string
+                                        se inicializan con null, por lo que en esta linea se esta
+                                        intentando acceder a la propiedad desde un objeto null => INVALIDO
+                                                                                */
+    }
 }
