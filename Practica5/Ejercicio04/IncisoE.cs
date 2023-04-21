c2b0ed822507f05e8e0c8b718dc7c23c5b587789
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 15:51:38 -0300

    Create IncisoE.cs

---
+++ Practica5/Ejercicio04/IncisoE.cs
@@ -0,0 +1,18 @@
+class A {
+    static int s_a = 0;
+    int a = 0;
+    static A() {
+        a = 30;
+    }
+    A(int a) {
+        a = s_a;
+    }
+}
+
+/*
+    La declaracion de la clase A es incorrecta. En cuanto a tiempos, lo primero que se inicializa es la variable estática
+    seguido por el constructor estático y luego lo demás; por ende no se puede asignar el valor "30" a la variable de
+    instancia "a" porque en ese momento la variable todavia no existe.
+    Además, intenta asignar una variable no estatica dentro del constructor estatico y en el constructor de instancia
+    intenta asignar un valor estatico a uno no estatico.
+*/
