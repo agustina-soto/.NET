72a97f1229ee5d16fef29a4f243560b2eb2d3036
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 15:15:36 -0300

    Create IncisoC.cs

---
+++ Practica5/Ejercicio04/IncisoC.cs
@@ -0,0 +1,15 @@
+class A {
+    static int s_a = 0;
+    static A(int a) {
+        s_a=a;
+    }
+    A(int a) {
+        s_a = a;
+    }
+}
+
+/*
+    La declaracion de la clase A es incorrecta. Un constructor estatico no puede tener parametros.
+    Además, el constructor de instancia no tiene un modificador de acceso declarado, por lo que se le asignará "private";
+    con lo cual no se podrán instanciar objetos de la clase A fuera de la misma.
+*/
