cb9b5ce9c0b6010138a5cd7225596a4a60b995f6
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 16:20:31 -0300

    Create IncisoK.cs

---
+++ Practica5/Ejercicio04/IncisoK.cs
@@ -0,0 +1,12 @@
+class A
+{
+    static int[] vector = { 1, 2, 3 };
+    public int this[int i]
+    {
+        get { return vector[i]; }
+    }
+}
+
+/*
+    La declaracion de la clase A es correcta. El indizador esta declarado correctamente
+*/
