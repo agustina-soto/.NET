d37c556a8b9be9e5f8d30895c9a2291ba9708b6c
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 16:07:23 -0300

    Create IncisoG.cs

---
+++ Practica5/Ejercicio04/IncisoG.cs
@@ -0,0 +1,9 @@
+class A
+{
+    const double PI = 3.1416;
+    static double DoblePI = 2 * PI;
+}
+
+/*
+    La declaracion de la clase A es correcta. Las constantes son implicitamente estaticas.
+*/
