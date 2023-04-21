6a3f3bc589a6b78e654dcc26e22b7fff66f04ace
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 20:16:38 -0300

    Update Program.cs

--- Practica5/Ejercicio05/Program.cs
+++ Practica5/Ejercicio05/Program.cs
@@ -1,1 +1,25 @@
-
+namespace A;
+class A
+{
+  char c;
+  static string st;
+  
+  void metodo1()
+  {
+    st = "string";
+    c = 'A';
+  }
+  
+  static void metodo2()
+  {
+    new ClaseA().c = 'a'; // ClaseA no existe
+    st = "st2";
+    c = 'B'; // c es una variable de instancia, no puede ser accedida dentro del metodo estatico
+    new A().st = "otro string";
+    /* No se puede acceder a una variable estática mediante una instancia de clase.
+       Ademas, el acceso a traves del punto se da para metodos y propiedades; st no esta
+       definida como propiedad ni como metodo, por lo que aunque no fuese estatica la linea
+       seria invailda.
+    */
+  }
+}
