d00efb93a7545ccf7f38f8b1cdea5ceb9df86afa
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 19:58:45 -0300

    Update Cuenta.cs

--- Practica5/Ejercicio03/Cuenta.cs
+++ Practica5/Ejercicio03/Cuenta.cs
@@ -1,66 +1,74 @@
-namespace ejercicio03;
+namespace ejercicio02;
 using System.Collections;
 
 class Cuenta{
     /* SOBRE LA INSTANCIA */
-    private int _Id {get; set;} // Id unico de la instancia
-    private double _Saldo {get; set;} // Saldo disponible de la instancia
+    private int Id {get; set;} // Id unico de la instancia
+    private double Saldo {get; set;} // Saldo disponible de la instancia
 
     /* SOBRE LA CLASE */
-    private static int s_CuentasCreadas = 0; // Cantidad de cuentas creadas
-    private static int s_CantDepositos = 0; // Cantidad de depositos realizados
-    private static int s_CantExtracciones = 0; // Cantidad de extracciones realizadas
-    private static double s_MontoDepositado = 0; // Monto total de depositos realizados
-    private static double s_MontoExtraido = 0; // Monto total de extracciones realizadas
-    private static int s_ExtraccionesInvalidas = 0; // Total extracciones invalidas (falta de fondos)
-    private static List<Cuenta> s_ListaCuentas = new List<Cuenta>();
+    private static int CuentasCreadas = 0; // Cantidad de cuentas creadas
+    private static int CantDepositos = 0; // Cantidad de depositos realizados
+    private static int CantExtracciones = 0; // Cantidad de extracciones realizadas
+    private static double MontoDepositado = 0; // Monto total de depositos realizados
+    private static double MontoExtraido = 0; // Monto total de extracciones realizadas
+    private static int ExtraccionesInvalidas = 0; // Total extracciones invalidas (falta de fondos)
+    private static List<Cuenta> ListaCuentas = new List<Cuenta>();
 
     public Cuenta(){
-        this._Id = ++Cuenta.s_CuentasCreadas; // Asigna Id a la instancia
-        Cuenta.s_ListaCuentas.Add(this); // Agrega la nueva cuenta a la lista de cuentas
-        Console.WriteLine($"Se creo la cuenta Id = {this._Id}");
+        this.Id = ++Cuenta.CuentasCreadas; // Asigna Id a la instancia
+        Cuenta.ListaCuentas.Add(this); // Agrega la nueva cuenta a la lista de cuentas
+        Console.WriteLine($"Se creo la cuenta Id = {this.Id}");
     }
 
     public void ActualizarListaSaldo(){
-        int indice = this._Id;
-        Cuenta.s_ListaCuentas[--indice]._Saldo = this._Saldo;
+        int indice = this.Id;
+        Cuenta.ListaCuentas[--indice].Saldo = this.Saldo;
     }
 
     public Cuenta Depositar(double monto){
-        this._Saldo += monto;
-        Cuenta.s_CantDepositos++;
-        Cuenta.s_MontoDepositado += monto;
+        this.Saldo += monto;
+        Cuenta.CantDepositos++;
+        Cuenta.MontoDepositado += monto;
         ActualizarListaSaldo();
-        Console.WriteLine($"Se deposito {monto} en la cuenta {this._Id} (Saldo = {this._Saldo})");
+        Console.WriteLine($"Se deposito {monto} en la cuenta {this.Id} (Saldo = {this.Saldo})");
         return this;
     }
 
     public Cuenta Extraer(double monto){
-        if (this._Saldo >= monto)
+        if (this.Saldo >= monto)
         {
-            this._Saldo -= monto;
-            Cuenta.s_CantExtracciones++;
-            Cuenta.s_MontoExtraido += monto;
+            this.Saldo -= monto;
+            Cuenta.CantExtracciones++;
+            Cuenta.MontoExtraido += monto;
             ActualizarListaSaldo();
-            Console.WriteLine($"Se extrajo {monto} de la cuenta {this._Id} (Saldo = {this._Saldo})");
+            Console.WriteLine($"Se extrajo {monto} de la cuenta {this.Id} (Saldo = {this.Saldo})");
         }
         else
         {
-            Cuenta.s_ExtraccionesInvalidas++;
-            Console.WriteLine($"Operación denegada - Saldo insuficiente: {this._Saldo} < {monto}");
+            Cuenta.ExtraccionesInvalidas++;
+            Console.WriteLine($"Operación denegada - Saldo insuficiente: {this.Saldo} < {monto}");
         }
         return this;
     }
 
     public static void ImprimirDetalle(){
         //Console.WriteLine($"CUENTAS CREADAS: {Cuenta.CuentasCreadas}\nDEPOSITOS:\t {Cuenta.CantDepositos}\t- Total depositado: \t{Cuenta.MontoDepositado}\nEXTRACCIONES:\t {Cuenta.CantExtracciones}\t- Total extraido: \t{Cuenta.MontoExtraido}\n\t\t\t- Saldo \t\t{Cuenta.MontoDepositado-Cuenta.MontoExtraido}\n* Se denegaron {Cuenta.ExtraccionesInvalidas} extracciones por falta de fondos");
-        Console.WriteLine($"CUENTAS CREADAS: {Cuenta.s_CuentasCreadas}");
-        Console.WriteLine($"DEPOSITOS:       {Cuenta.s_CantDepositos}     - Total depositado: {Cuenta.s_MontoDepositado}");
-        Console.WriteLine($"EXTRACCIONES:    {Cuenta.s_CantExtracciones}     - Total extraido:   {Cuenta.s_MontoExtraido}");
-        Console.WriteLine($"                       - Saldo             {Cuenta.s_MontoDepositado - Cuenta.s_MontoExtraido}");
-        Console.WriteLine($"Se denegaron {Cuenta.s_ExtraccionesInvalidas} extracciones por falta de fondos");
+        Console.WriteLine($"CUENTAS CREADAS: {Cuenta.CuentasCreadas}");
+        Console.WriteLine($"DEPOSITOS:       {Cuenta.CantDepositos}     - Total depositado: {Cuenta.MontoDepositado}");
+        Console.WriteLine($"EXTRACCIONES:    {Cuenta.CantExtracciones}     - Total extraido:   {Cuenta.MontoExtraido}");
+        Console.WriteLine($"                       - Saldo             {Cuenta.MontoDepositado - Cuenta.MontoExtraido}");
+        Console.WriteLine($"Se denegaron {Cuenta.ExtraccionesInvalidas} extracciones por falta de fondos");
     }
 
-    public static List<Cuenta> GetCuentas => Cuenta.s_ListaCuentas;
 
+    public static List<Cuenta> GetCuentas {
+        get {
+            List<Cuenta> lista = new List<Cuenta>();
+            foreach (Cuenta c in Cuenta.ListaCuentas){
+                lista.Add(c);
+            }
+            return lista;
+        }
+    }
 }
