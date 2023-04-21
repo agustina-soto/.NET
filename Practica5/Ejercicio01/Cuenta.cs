e4745a3a4ab28c7dd51a8622701950bfb8c49dc0
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-21 12:00:34 -0300

    Create Cuenta.cs

---
+++ Practica5/Ejercicio01/Cuenta.cs
@@ -0,0 +1,54 @@
+namespace ejercicio01;
+class Cuenta{
+    /* SOBRE LA INSTANCIA */
+    private int Id {get; set;} // Id unico de la instancia
+    private double Saldo {get; set;} // Saldo disponible de la instancia
+
+    /* SOBRE LA CLASE */
+    private static int CuentasCreadas = 0; // Cantidad de cuentas creadas
+    private static int CantDepositos = 0; // Cantidad de depositos realizados
+    private static int CantExtracciones = 0; // Cantidad de extracciones realizadas
+    private static double MontoDepositado = 0; // Monto total de depositos realizados
+    private static double MontoExtraido = 0; // Monto total de extracciones realizadas
+    private static int ExtraccionesInvalidas = 0; // Total extracciones invalidas (falta de fondos)
+
+    public Cuenta(){
+        Cuenta.CuentasCreadas++;
+        this.Id = Cuenta.CuentasCreadas; // osea this.Id = ++Cuenta.CuentasCreadas ??
+        Console.WriteLine($"Se creo la cuenta Id = {Cuenta.CuentasCreadas}");
+    }
+
+    public Cuenta Depositar(double monto){
+        this.Saldo += monto;
+        Cuenta.CantDepositos++;
+        Cuenta.MontoDepositado += monto;
+        Console.WriteLine($"Se deposito {monto} en la cuenta {Cuenta.CuentasCreadas} (Saldo = {this.Saldo})");
+        return this;
+    }
+
+    public Cuenta Extraer(double monto){
+        if (this.Saldo >= monto)
+        {
+            this.Saldo -= monto;
+            Cuenta.CantExtracciones++;
+            Cuenta.MontoExtraido += monto;
+            Console.WriteLine($"Se extrajo {monto} de la cuenta {Cuenta.CuentasCreadas} (Saldo = {this.Saldo})");
+        }
+        else
+        {
+            Cuenta.ExtraccionesInvalidas++;
+            Console.WriteLine($"Operación denegada - Saldo insuficiente --> {monto} > {this.Saldo}");
+        }
+        return this;
+    }
+
+    public static void ImprimirDetalle(){
+        //Console.WriteLine($"CUENTAS CREADAS: {Cuenta.CuentasCreadas}\nDEPOSITOS:\t {Cuenta.CantDepositos}\t- Total depositado: \t{Cuenta.MontoDepositado}\nEXTRACCIONES:\t {Cuenta.CantExtracciones}\t- Total extraido: \t{Cuenta.MontoExtraido}\n\t\t\t- Saldo \t\t{Cuenta.MontoDepositado-Cuenta.MontoExtraido}\n* Se denegaron {Cuenta.ExtraccionesInvalidas} extracciones por falta de fondos");
+        Console.WriteLine($"CUENTAS CREADAS: {Cuenta.CuentasCreadas}");
+        Console.WriteLine($"DEPOSITOS:       {Cuenta.CantDepositos}     - Total depositado: {Cuenta.MontoDepositado}");
+        Console.WriteLine($"EXTRACCIONES:    {Cuenta.CantExtracciones}     - Total extraido:   {Cuenta.MontoExtraido}");
+        Console.WriteLine($"                       - Saldo             {Cuenta.MontoDepositado - Cuenta.MontoExtraido}");
+        Console.WriteLine($"Se denegaron {Cuenta.ExtraccionesInvalidas} extracciones por falta de fondos");
+    }
+
+}
