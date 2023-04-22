e15076322d74610f9c327518c933102bc0056e1d
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 14:00:39 -0300

    Update Program.cs

--- Practica5/Ejercicio09/Program.cs
+++ Practica5/Ejercicio09/Program.cs
@@ -1,1 +1,26 @@
+// ¿Cuál es el error en el siguiente programa?
 
+Auto a = new Auto();
+a.Marca = "Ford";
+Console.WriteLine(a.Marca);
+class Auto
+{
+    private string marca;
+    public string Marca
+    {
+        set
+        {
+            Marca = value;
+        }
+        get
+        {
+            return marca;
+        }
+    }
+}
+
+/*
+    El error esta en la linea 11 (Marca = value), porque no setea la variable de instancia "marca" sino
+    la propiedad "Marca". Como dice la nota de la consigna: utilizar la convención de prefijar a los campos
+    privados con guión bajo hace más difícil cometer este tipo de errores.
+*/
