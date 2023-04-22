eeeb49f047909aca13c86e90a8cbdb681e21d339
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 12:18:58 -0300

    Update IncisoD.cs

--- Practica5/Ejercicio04/IncisoD.cs
+++ Practica5/Ejercicio04/IncisoD.cs
@@ -15,8 +15,3 @@ class A {
     ADVERTENCIA: El constructor de instancia (al no tener explicito el modificador de acceso)
     es private, por lo que no se van a poder instanciar objetos fuera de a clase.
 */
-
-/*
-esto al final no esta bien no?
-Quiere hacer la asignacion de una variable de instancia estatica dentro de un metodo no estatico, algo que no está permitido.
-*/
