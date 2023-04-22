44e1be046de1986f6225cc9783b6bb737e665a0e
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 15:15:16 -0300

    Create ejercicio11.cs

---
+++ Practica5/Ejercicio11/ejercicio11.cs
@@ -0,0 +1,5 @@
+// ¿ Qué diferencia hay entre estas dos declaraciones?
+
+public int X = 3; // Es una variable publica de tipo entera a la que se le esta asignando un valor
+
+public int X => 3; // Es una propiedad publica de tipo entera la cual devuelve el valor que se le indica
