2683bf716a3c4eeb2b7c0f54a21624a195eeecbe
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 22:52:35 -0300

    Update and rename Inciso2.cs to Inciso02.cs

--- Practica6/Ejercicio06/Inciso02.cs
+++ Practica6/Ejercicio06/Inciso02.cs
@@ -9,5 +9,5 @@ public override string M1()
 }
 
 /*
-  El error esta en que se declara una variable abstracta (M1) en una clase no abstracta (A)
+  El error esta en que se declara un metodo abstracto (M1) en una clase no abstracta (A)
 */
