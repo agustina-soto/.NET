bd6344fefb76e5aaf6a942a17a707b894bc4a027
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 23:40:15 -0300

    Update Inciso12.cs

--- Practica6/Ejercicio06/Inciso12.cs
+++ Practica6/Ejercicio06/Inciso12.cs
@@ -1,1 +1,17 @@
+abstract class A
+{
+    public int Prop {set; get;}
+}
 
+
+class B : A
+{
+    public override int Prop {
+    get => 5;
+    set {}
+    }
+}
+
+/*
+    El error esta en que una propiedad no se puede invalidar. Para hacerlo, hace falta usar la palabra clave virtual en el encabezado de la declaracion de la propiedad.
+*/
