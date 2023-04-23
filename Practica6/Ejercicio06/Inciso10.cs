8a4ffd62895b47b75ec065cd58aeb90d3719cf5e
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 23:26:19 -0300

    Update Inciso10.cs

--- Practica6/Ejercicio06/Inciso10.cs
+++ Practica6/Ejercicio06/Inciso10.cs
@@ -1,1 +1,15 @@
+class A
+{
+    private int Prop
+    { set; public get; }
+}
 
+
+class B : A
+{
+}
+
+/*
+    Los descriptores de acceso (get-set) pueden tener distintos niveles de accesibilidad, pero el modificador del accessor debe ser más restrictivo que el de la propiedad.
+    El descriptor de acceso de lectura se definio como public mientras que el de la propiedad se declaro como private.
+*/
