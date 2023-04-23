49f744a23f090c065b4a61dc8098eaede5ad5764
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-22 23:35:37 -0300

    Update Inciso11.cs

--- Practica6/Ejercicio06/Inciso11.cs
+++ Practica6/Ejercicio06/Inciso11.cs
@@ -1,1 +1,17 @@
+abstract class A
+{
+    public abstract int Prop {set; get;}
+}
 
+
+class B : A
+{
+    public override int Prop
+    {
+        get => 5;
+    }
+}
+
+/*
+    El error esta en que falta implementar en la clase B el descriptor de acceso de escritura de la propiedad abstracta definida en A.
+*/
