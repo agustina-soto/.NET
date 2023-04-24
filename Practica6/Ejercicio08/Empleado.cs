8bf29dccb588e83c36b92f3ce823797698316609
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 00:33:50 -0300

    Update Empleado.cs

--- Practica6/Ejercicio08/Empleado.cs
+++ Practica6/Ejercicio08/Empleado.cs
@@ -2,19 +2,14 @@ namespace ejercicio08;
 
 abstract class Empleado{
 
-    protected string _nombre;
-    protected int _DNI;
-    protected DateTime _fechaIngreso;
-    protected decimal _salarioBase;
-    protected decimal _salario;
-
-    public string Nombre { get; }
+    public string? Nombre { get; }
     public int DNI { get; }
-    public DateTime FechaDeIngreso { get; }
+    public DateTime FechaIngreso { get; }
     public decimal SalarioBase { get; }
-    public decimal Salario{ get; } // Salario base más la comisión o el premio según corresponda.
+    public decimal Salario{ get; }
 
 
     public abstract void AumentarSalario();
 
+    public abstract override string ToString();
 }
