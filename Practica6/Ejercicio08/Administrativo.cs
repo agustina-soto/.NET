3bfb13c574d65ced582887e52de20375b9af986f
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 00:34:00 -0300

    Update Administrativo.cs

--- Practica6/Ejercicio08/Administrativo.cs
+++ Practica6/Ejercicio08/Administrativo.cs
@@ -4,17 +4,32 @@ class Administrativo : Empleado {
 
     public decimal Premio { get; set; }
 
+    // Oculta las propiedaded originales de la superclase para definir una nueva implementacion de cada una --> desde la clase Empleado se sigue pudiendo acceder a los descriptores de lectura
+    new public string? Nombre { get; set; }
+    new public int DNI { get; set; }
+    new public DateTime FechaIngreso { get; set; }
+    new public decimal SalarioBase { get; set; }
+    new public decimal Salario { get; set; }
+
+
     public Administrativo(string nombre, int dni, DateTime fechaIngreso, decimal salarioBase){
-        _nombre = nombre;
-        _DNI = dni;
-        _fechaIngreso = fechaIngreso;
-        _salarioBase = salarioBase;
-        _salario = _salarioBase + Premio;
+        Nombre = nombre;
+        DNI = dni;
+        FechaIngreso = fechaIngreso;
+        SalarioBase = salarioBase;
+        Salario = SalarioBase + Premio;
     }
 
+
     // Incrementa el salario base en un 1% por cada año de antigüedad
-    public override void AumentarSalario(){ // num * % / 100
-        int antiguedad = DateTime.Now.Year - _fechaIngreso.Year;
-        Premio = (_salarioBase*1/100)*antiguedad;
+    public override void AumentarSalario(){
+        int antiguedad = DateTime.Now.Year - FechaIngreso.Year;
+        Premio = (SalarioBase*1/100)*antiguedad;
+        Salario += Premio;
+    }
+
+
+    public override string ToString(){
+        return "Administrativo " + $"Nombre: {Nombre}, DNI: {DNI} Antigüedad: {DateTime.Now.Year - FechaIngreso.Year}\nSalario base: {SalarioBase}, Salario: {Salario}\n-------------";
     }
 }
