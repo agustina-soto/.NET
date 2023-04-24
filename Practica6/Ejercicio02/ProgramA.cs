36d778d4eafb02b03d6a7c9e12b19d42f8a81730
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 01:12:49 -0300

    Update ProgramA.cs

--- Practica6/Ejercicio02/ProgramA.cs
+++ Practica6/Ejercicio02/ProgramA.cs
@@ -1,1 +1,12 @@
+// Utilizando el operador is
 
+using System;
+using ejercicio02;
+
+A[] vector = new A[] { new C(1), new D(2), new B(3), new D(4), new B(5) };
+foreach (A a in vector)
+{
+    if ( (! (a is C)) & (! (a is D) ) || (! (a is A) ) ){ // || (! (a is A) esta de mas no?
+        a.Imprimir();
+    }
+}
