203c4574fb2f183763a9b783f6e96a9b750843f4
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 01:17:01 -0300

    Update ProgramaB.cs

--- Practica6/Ejercicio02/ProgramaB.cs
+++ Practica6/Ejercicio02/ProgramaB.cs
@@ -1,1 +1,10 @@
+// Utilizando el método GetType() y el operador typeof()
 
+using System;
+using ejercicio02;
+
+A[] vector = new A[] { new C(1), new D(2), new B(3), new D(4), new B(5) };
+foreach (A a in vector)
+{
+    if (a.GetType() == typeof(B)) a.Imprimir();
+}
