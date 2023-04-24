c794db7668f1cc05bc78d25e6489501fce5f964a
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 00:35:14 -0300

    Create Clases.cs

---
+++ Practica6/Ejercicio01/Clases.cs
@@ -0,0 +1,49 @@
+using System;
+namespace ejercicio01;
+
+class A
+{
+    protected int _id;
+    public A(int id) => _id = id;
+    public virtual void Imprimir() => Console.WriteLine($"A_{_id}");
+}
+
+// ----------------------------------------------------------------------------------------------
+
+class B : A
+{
+    public B(int id) : base(id) => base._id = id;
+
+    public override void Imprimir()
+    {
+        Console.Write($"B_{_id} --> ");
+        base.Imprimir();
+    }
+}
+
+// ----------------------------------------------------------------------------------------------
+
+class C : B
+{
+    public C(int id) : base(id) => base._id = id;
+
+    public override void Imprimir()
+    {
+        Console.Write($"C_{_id} --> ");
+        base.Imprimir();
+    }
+}
+
+// ----------------------------------------------------------------------------------------------
+
+class D : C
+{
+    public D(int id) : base(id) => base._id = id;
+    
+    public override void Imprimir()
+    {
+        Console.Write($"D_{_id} --> ");
+        base.Imprimir();
+    }
+
+}
