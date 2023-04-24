152eac1e08fc197bc902c01d4ae32174b74ca9de
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-24 00:34:14 -0300

    Update Vendedor.cs

--- Practica6/Ejercicio08/Vendedor.cs
+++ Practica6/Ejercicio08/Vendedor.cs
@@ -4,18 +4,32 @@ class Vendedor : Empleado {
 
     public decimal Comision { get; set; }
 
+    // Oculta las propiedaded originales de la superclase para definir una nueva implementacion de cada una --> desde la clase Empleado se sigue pudiendo acceder a los descriptores de lectura
+    new public string? Nombre { get; set; }
+    new public int DNI { get; set; }
+    new public DateTime FechaIngreso { get; set; }
+    new public decimal SalarioBase { get; set; }
+    new public decimal Salario { get; set; }
+
+
     public Vendedor(string nombre, int dni, DateTime fechaIngreso, decimal salarioBase) {
-        _nombre = nombre;
-        _DNI = dni;
-        _fechaIngreso = fechaIngreso;
-        _salarioBase = salarioBase;
-        _salario = _salarioBase + Comision;
+        Nombre = nombre;
+        DNI = dni;
+        FechaIngreso = fechaIngreso;
+        SalarioBase = salarioBase;
+        Salario = SalarioBase + Comision;
     }
+
 
     // Incrementa el salario base en un 5% si su antigüedad es inferior a 10 años o en un 10% en caso contrario.
     public override void AumentarSalario(){
-        int antiguedad = DateTime.Now.Year - _fechaIngreso.Year;
-        Comision = (antiguedad < 10) ? (_salarioBase*5/100) : (_salarioBase*10/100);
+        int antiguedad = DateTime.Now.Year - FechaIngreso.Year;
+        Comision = (antiguedad < 10) ? (SalarioBase*5/100) : (SalarioBase*10/100);
+        Salario += Comision;
     }
 
+
+    public override string ToString(){
+        return "Vendedor " + $"Nombre: {Nombre}, DNI: {DNI} Antigüedad: {DateTime.Now.Year - FechaIngreso.Year}\nSalario base: {SalarioBase}, Salario: {Salario}\n-------------";
+    }
 }
