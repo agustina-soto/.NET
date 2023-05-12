5879eabdc747b359d95143239fc43554f405684b
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-12 19:03:49 -0300

    Update Procesador.cs

--- TP7/Ejercicio01/Procesador.cs
+++ TP7/Ejercicio01/Procesador.cs
@@ -1,8 +1,8 @@
-namespace ejercicio01;
+namespace tp7;
 static class Procesador
 {
     public static void Alquilar(IAlquilable x, Persona p) => x.SeAlquilaA(p);
-    public static void Devolver(IAlquilable x, Persona p) => x.SeAlquilaA(p);
+    public static void Devolver(IAlquilable x, Persona p) => x.DevueltoPor(p);
     public static void Vender(IVendible x, Persona p) => x.SeVendeA(p);
     public static void Atender(IAtendible x) => x.SeAtiendeA();
     public static void Lavar(ILavable x) => x.Lavando();
