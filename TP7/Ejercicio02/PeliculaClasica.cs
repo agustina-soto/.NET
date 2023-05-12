350008f3251977f419e6257925e5bcd35cfa0e15
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-12 19:04:35 -0300

    Update PeliculaClasica.cs

--- TP7/Ejercicio02/PeliculaClasica.cs
+++ TP7/Ejercicio02/PeliculaClasica.cs
@@ -1,9 +1,11 @@
-namespace ejercicio01;
+namespace tp7;
 
 class PeliculaClasica : Pelicula , IAlquilable, IVendible {
 
     public PeliculaClasica() : base() { }
 
-    public void SeVendeA(Persona p) => Console.WriteLine("");
+    public void SeVendeA(Persona p) => Console.WriteLine("Vendiendo película clásica a persona");
+    public override void SeAlquilaA(Persona p) => Console.WriteLine("Alquilando pelicula clasica a persona");
+    public override void DevueltoPor(Persona p) => Console.WriteLine("Pelicula clasica devuelta por persona");
 
 }
