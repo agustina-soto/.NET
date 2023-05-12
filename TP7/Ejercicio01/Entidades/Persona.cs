41ef1036d42f1b6c856ebd6d0e6484e20d38ef8f
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-12 20:22:19 -0300

    Update Persona.cs

--- TP7/Ejercicio01/Entidades/Persona.cs
+++ TP7/Ejercicio01/Entidades/Persona.cs
@@ -1,10 +1,13 @@
-namespace ejercicio01;
+namespace tp7;
 
-class Persona : IAtendible {
+class Persona : IAtendible, IImportante, IComercial{
 
     public Persona(){
     }
 
     public void SeAtiendeA() => Console.WriteLine("Atendiendo a persona");
 
+    public void Importar() => Console.WriteLine("Método Importar() de la clase Persona");
+    
+    // NO FUNCIONA, TENGO PROBLEMAS CON LAS NUEVAS INTERFACES
 }
