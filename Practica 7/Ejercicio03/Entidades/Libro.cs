b3e06cba95c09d6518bc6b471a9d135e106ae95f
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-13 01:46:32 -0300

    Update Libro.cs

--- Practica 7/Ejercicio03/Entidades/Libro.cs
+++ Practica 7/Ejercicio03/Entidades/Libro.cs
@@ -2,10 +2,6 @@ namespace tp7;
 
 class Libro : IAlquilable, IReciblable {
 
-    public Libro(){
-        
-    }
-
     public void SeAlquilaA(Persona p) => Console.WriteLine("Alquilando libro a persona");
 
     public void DevueltoPor(Persona p) => Console.WriteLine("Libro devuelto por persona");
