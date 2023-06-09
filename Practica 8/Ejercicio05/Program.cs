8a77478e2cb55c27e1dcdfd8492b132bbd9f306e
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-06-09 15:25:36 -0300

    Create Program.cs

---
+++ Practica 8/Ejercicio05/Program.cs
@@ -0,0 +1,9 @@
+using ejercicio05;
+
+int[] vector = new int[] { 1, 2, 3, 4, 5 };
+
+vector.Print("Valores iniciales: ");
+
+vector.Donde(n => n % 2 == 0).Print("Pares: ");
+
+vector.Donde(n => n % 2 == 1).Seleccionar(n => n * n).Print("Impares al cuadrado: ");
